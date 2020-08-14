127e2eb3437f1d0702426d84ef0caacdef9a15ed
yexiaoyugan <7842596+yexiaoyugan@user.noreply.gitee.com>
2020-08-14 09:37:45 +0800

    bqb

---
+++ yelou/bqb/bqb_ty37.js
@@ -0,0 +1,30 @@
+
+// 封装异步加载资源的方法
+function loadExternalResource(url, type) {
+    return new Promise((resolve, reject) => {
+        let tag;
+
+        if (type === "css") {
+            tag = document.createElement("link");
+            tag.rel = "stylesheet";
+            tag.href = url;
+        }
+        else if (type === "js") {
+            tag = document.createElement("script");
+            tag.src = url;
+        }
+        if (tag) {
+            tag.onload = () => resolve(url);
+            tag.onerror = () => reject(url);
+            document.head.appendChild(tag);
+        }
+    });
+}
+
+var judge12 = window.location.href;
+if(judge12.indexOf("bilibilipy")!=-1&&judge12.indexOf("https")!=-1){
+
+    loadExternalResource("https://cdn.jsdelivr.net/gh/mizhiyugan529/resource@3.7/yelou/bqb/bqb2.js", "js");
+}else{
+    loadExternalResource("https://cdn.jsdelivr.net/gh/mizhiyugan529/resource@3.7/yelou/bqb/bqb.js", "js");
+}
